ed5bb78523cb1b516f15b13f2881eb199c5fd3e4
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-07-31 20:24:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 from pydantic import BaseModel, Field
 from agno.agent import Agent 
 from agno.models.openai import OpenAIChat
-from firecrawl import FirecrawlApp 
+from firecrawl import FirecrawlApp
 import streamlit as st 
 import os
 from dotenv import load_dotenv 
